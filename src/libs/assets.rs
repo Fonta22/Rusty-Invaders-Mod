4645e99993a711dde7cbfd3bcc0df7d7c1e33657
Fonta22 <paufontasp@gmail.com>
2024-02-25 01:22:08 +0100

    improve resolution & add new sprites

--- src/libs/assets.rs
+++ src/libs/assets.rs
@@ -1,12 +1,14 @@
-pub const PLAYER_TEXTURE_NAME: &str = "ship.png";
-pub const BULLET_TEXTURE_NAME: &str = "bullet.png";
-pub const BACKGROUND_TEXTURE_NAME: &str = "space.jpg";
+pub const PLAYER_TEXTURE_NAME: &str = "pda.png";
+pub const BULLET_TEXTURE_NAME: &str = "energy.png";
+pub const BACKGROUND_TEXTURE_NAME: &str = "background_-1.jpg";
 
-pub const CRAB_TEXTURE_NAME: &str = "crab.png";
-pub const SQUID_TEXTURE_NAME: &str = "squid.png";
-pub const OCTOPUS_TEXTURE_NAME: &str = "octopus.png";
+pub const CRAB_TEXTURE_NAME: &str = "freedom.png";
+pub const SQUID_TEXTURE_NAME: &str = "bandits.png";
+pub const OCTOPUS_TEXTURE_NAME: &str = "duty.png";
 
 pub const PLAYER_EXPLOSION_SOUND_NAME: &str = "player_explosion.wav";
 pub const ENEMY_EXPLOSION_SOUND_NAME: &str = "enemy_explosion.wav";
 pub const PLAYER_HIT_SOUND_NAME: &str = "hit.wav";
 pub const PLAYER_LASER_SHOOT_SOUND_NAME: &str = "laser_shoot.wav";
+
+pub const FONT_NAME: &str = "AmazS.T.A.L.K.E.R.v.3.0.ttf";
